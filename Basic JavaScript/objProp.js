185a474c2915e5eb4afb5191d0cef974711d2812
arsen056 <vurdihanov@gmail.com>
2022-09-09 22:09:32 +0300

    feat: add solve tasks

---
+++ Basic JavaScript/objProp.js
@@ -0,0 +1,21 @@
+// Setup
+const testObj = {
+  hat: 'ballcap',
+  shirt: 'jersey',
+  shoes: 'cleats',
+};
+
+// Only change code below this line
+const hatValue = testObj.hat; // Change this line
+const shirtValue = testObj.shirt; // Change this line
+
+// Setup
+const testObjBr = {
+  'an entree': 'hamburger',
+  'my side': 'veggies',
+  'the drink': 'water',
+};
+
+// Only change code below this line
+const entreeValue = testObjBr['an entree']; // Change this line
+const drinkValue = testObjBr['the drink']; // Change this line
